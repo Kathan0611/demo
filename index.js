ecd6c5756b5be3b839e6c1d8fa286fb0781e2380
Kathan0611 <90961728+Kathan0611@users.noreply.github.com>
2024-02-24 23:18:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express =require('express');
 const app = require("express")();
 const mogoose = require("mongoose");
-const body=require('express');
+
 require("dotenv").config();
 const {CreateProduct}=require('./Controller/ProductController');
 const Router =require('./Router/ProductRouter');
-const ex= require('express');
+
 
 //in req.body part if data not json then convert into json
 app.use(express.json());
 app.use("/product",Router);
 main().catch((err) => console.log(err));
-app.use('body-parser');
+
 async function main() {
   await mogoose.connect("mongodb://127.0.0.1:27017/test");
   console.log("connect database succefully");
